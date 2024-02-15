378baf567bb34a370761834414b28c5a377dd1d5
nioliu <nioliunio@gmail.com>
2024-02-15 16:04:40 -0500

    sme basic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import App from "./App.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import ProduceUp from "./components/ProduceUpded.vue";
 import Uploadweb from "./components/uploadPro.vue";
 import ProduceIn from "./components/ProduceInfo.vue";
@@ -10,28 +10,30 @@ import userInf from "./components/userInformation.vue";
 import ProduceList from "./components/ProduceList.vue";
 import interPro from "./components/InteProduct.vue";
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 let token = JSON.parse(localStorage.getItem("token"));
 if (token) {
-  console.log(token);
-  axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+    console.log(token);
+    axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 }
 axios.defaults.baseURL = "http://api.saveplus.link";
 
 const routes = [
-  { path: "/produceUp", component: ProduceUp },
-  { path: "/uploadPro", component: Uploadweb },
-  { path: "/pro-info", component: ProduceIn },
-  { path: "/signup", component: SignUp },
-  { path: "/signin", component: SignIn },
-  { path: "/p-information", component: userInf },
-  { path: "", component: ProduceList },
-  { path: "/interest", component: interPro },
+    {path: "/produceUp", component: ProduceUp},
+    {path: "/uploadPro", component: Uploadweb},
+    {name: "proInfo", path: "/proInfo", component: ProduceIn},
+    {path: "/signup", component: SignUp},
+    {path: "/signin", component: SignIn},
+    {path: "/p-information", component: userInf},
+    {path: "", component: ProduceList},
+    {path: "/interest", component: interPro},
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 const app = createApp(App);

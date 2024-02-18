8ab716d90e34cd95e6984f615ea06abf2c3042b6
Jiacheng <jcding@bu.edu>
2024-02-18 13:18:30 -0500

    upload list

--- src/utils/product.info.js
+++ src/utils/product.info.js
@@ -3,7 +3,17 @@ import {v4} from 'uuid'
 
 const baseUrl = "http://saveplus.link"
 
-
+/**
+ * 添加trace id
+ */
+axios.interceptors.request.use(function (config) {
+    config.headers.set('X-Trace-Id', v4())
+    config.withCredentials = true
+    return config
+},function (error) {
+    // 请求错误时做些什么
+    return Promise.reject(error);
+});
 
 /**
  * 获取商品的详细信息
@@ -32,7 +42,7 @@ export async function getProductList(pageNum) {
         url: baseUrl + "/post/list",
         params: {
             page_size: 10,
-            page_num: 1,
+            page_num: pageNum,
         }
     })
 }
